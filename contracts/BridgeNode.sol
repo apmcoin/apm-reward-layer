a3214b7fee3a224086be192e254471ed0f8096ad
john-ver <46668752+john-ver@users.noreply.github.com>
2023-12-15 06:16:04 +0900

    상위 레이어 앵커링 브릿지 계약

---
+++ contracts/BridgeNode.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./openzeppelin-contracts/access/AccessControl.sol";
+
+//상위 레이어 앵커링을 위한 브릿지 노드 계약
+contract BridgeNode is AccessControl {
+    bytes32 public constant WRITER_ROLE = keccak256("WRITER_ROLE");
+    event BlockHashRecorded(uint256 indexed blockNumber, bytes32 blockHash);
+
+    mapping(uint256 => bytes32) public blockHashes;
+    address[] private writers;
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function recordBlockHash(uint256 blockNumber, bytes32 blockHash) public onlyRole(WRITER_ROLE) {
+        blockHashes[blockNumber] = blockHash;
+        emit BlockHashRecorded(blockNumber, blockHash);
+    }
+
+    function getBlockHash(uint256 blockNumber) public view returns (bytes32) {
+        return blockHashes[blockNumber];
+    }
+
+    // Writer 목록에 주소 추가 및 권한 부여
+    function grantWriteAccess(address writer) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(WRITER_ROLE, writer);
+        writers.push(writer);
+    }
+
+    // Writer 목록에서 주소 제거 및 권한 취소
+    function revokeWriteAccess(address writer) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(WRITER_ROLE, writer);
+        removeWriter(writer);
+    }
+
+    // Writer 목록 조회
+    function getWriters() public view returns (address[] memory) {
+        return writers;
+    }
+
+    // Writer 목록에서 주소 제거 (내부 함수)
+    function removeWriter(address writer) internal {
+        uint length = writers.length;
+        for (uint i = 0; i < length; i++) {
+            if (writers[i] == writer) {
+                writers[i] = writers[length - 1];
+                writers.pop();
+                break;
+            }
+        }
+    }
+}
