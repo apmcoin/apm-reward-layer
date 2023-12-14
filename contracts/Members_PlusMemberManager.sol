0a7047da9e8ea047704c7589473e32957b19fb3d
john-ver <46668752+john-ver@users.noreply.github.com>
2023-12-15 07:00:35 +0900

    Add Plus Member Manager

---
+++ contracts/Members_PlusMemberManager.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.6;
+
+import "./rapm-contracts/access/Roles.sol";
+
+contract Members_PlusMemberManager {
+    using Roles for Roles.Role;
+
+    Roles.Role private owners;
+    Roles.Role private managers;
+    mapping(bytes32 => bool) private plusMembers;
+    bytes32[] private memberList; // Array to store all member UUIDs
+
+    event ManagerAdded(address indexed account);
+    event ManagerRemoved(address indexed account);
+    event PlusMemberAdded(bytes32 uuid);
+    event PlusMemberRemoved(bytes32 uuid);
+
+    constructor() public {
+        owners.add(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(owners.has(msg.sender), "Only owner can call this function.");
+        _;
+    }
+
+    modifier onlyManager() {
+        require(managers.has(msg.sender), "Only manager can call this function.");
+        _;
+    }
+
+    function addManager(address account) public onlyOwner {
+        managers.add(account);
+        emit ManagerAdded(account);
+    }
+
+    function removeManager(address account) public onlyOwner {
+        managers.remove(account);
+        emit ManagerRemoved(account);
+    }
+
+    function addPlusMembers(bytes32[] memory uuids) public onlyManager {
+        for (uint i = 0; i < uuids.length; i++) {
+            if (!plusMembers[uuids[i]]) {
+                plusMembers[uuids[i]] = true;
+                memberList.push(uuids[i]);
+                emit PlusMemberAdded(uuids[i]);
+            }
+        }
+    }
+
+    function removePlusMember(bytes32 uuid) public onlyManager {
+        require(plusMembers[uuid], "Member not found.");
+        plusMembers[uuid] = false;
+        emit PlusMemberRemoved(uuid);
+    }
+
+    function getPlusMembers() public view returns (bytes32[] memory) {
+        return memberList;
+    }
+
+    // Additional functions for role checks, etc.
+}
