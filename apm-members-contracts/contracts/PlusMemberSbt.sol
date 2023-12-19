8cbba9296b97e7cddc0e0980686d1a433c6219db
apmJSW <jeongsw@apm-coin.com>
2023-12-19 17:54:54 +0900

    feat: PlusMemberSBT가 ManagerRole을 상속하여 onlyManager 체크

--- apm-members-contracts/contracts/PlusMemberSbt.sol
+++ apm-members-contracts/contracts/PlusMemberSbt.sol
@@ -1,34 +1,34 @@
 pragma solidity ^0.5.6;
 
 import "./rapm-contracts/token/ERC721/ERC721.sol";
-import "./rapm-contracts/ownership/Ownable.sol";
+import "./ManagerRole.sol";
 
-contract ApmMembersPlusSBT is ERC721, Ownable {
+contract PlusMemberSBT is ERC721, ManagerRole {
     using SafeMath for uint256;
 
     uint256 private _tokenIds; // 토큰 ID 카운터
 
-    modifier onlySBTManager() {
-        require(msg.sender == owner(), "ApMMembersPlusSBT: caller is not the SBT manager");
-        _;
-    }
+    // modifier onlySBTManager() {
+    //     require(msg.sender == owner(), "ApMMembersPlusSBT: caller is not the SBT manager");
+    //     _;
+    // }
 
     constructor() public ERC721() {}
 
-    function mintMultiple(address[] memory to, uint256[] memory tokenIds) public onlySBTManager {
+    function mintMultiple(address[] memory to, uint256[] memory tokenIds) public onlyManager {
         require(to.length == tokenIds.length, "ApMMembersPlusSBT: to and tokenIds length mismatch");
         for (uint256 i = 0; i < to.length; i++) {
             _mint(to[i], tokenIds[i]);
         }
     }
 
-    function burnMultiple(uint256[] memory tokenIds) public onlySBTManager {
+    function burnMultiple(uint256[] memory tokenIds) public onlyManager {
         for (uint256 i = 0; i < tokenIds.length; i++) {
             _burn(tokenIds[i]);
         }
     }
 
-    function mintNext(address to) public onlySBTManager returns (uint256) {
+    function mintNext(address to) public onlyManager returns (uint256) {
         uint256 newTokenId = _tokenIds.add(1);
         _tokenIds = newTokenId;
         _mint(to, newTokenId);
