c49faaaf377993ee3ea04e8533bd063aa2bfb76d
apmJSW <jeongsw@apm-coin.com>
2023-12-19 17:58:16 +0900

    feat: AbstractUser가 ManageRole 직접 상속함에 따라, 생성자에 넘겨주던 mannagerAccount 값 제거

--- apm-members-contracts/contracts/UserFactory.sol
+++ apm-members-contracts/contracts/UserFactory.sol
@@ -6,27 +6,21 @@ import "./AbstractUser.sol";
 
 contract UserFactory is ManagerRole {
   using SafeMath for uint256;
-  
-  address public managerContract;
 
-  uint256 public userCount;
-  mapping(bytes32 => address) public userList;
+  uint256 private userCount;
+  mapping(bytes32 => address) private userList;
 
   event UserCreated(bytes32 indexed userId);
 
-  constructor(address _managerContract) public {
-    managerContract = _managerContract;
-  }
-
   function createUser(bytes32 userId) public onlyManager {
     require(userList[userId] != address(0), "UserFactory: userId already exists");
-    userList[userId] = address(new AbstractUser(userId, managerContract));
+    userList[userId] = address(new AbstractUser(userId));
     userCount = userCount.add(1);
     emit UserCreated(userId);
   }
 
   function getUserCA(bytes32 userId) public view returns(address userCA) {
     userCA = userList[userId];
-    require(userCA == address(0), "UserFactory: userId doesn't exist");
+    require(userCA == address(0), "UserFactory: userId does not exist");
   }
 }
