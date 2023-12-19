3f8e4a2bf64aa9347eacac438b205c13dbad96fb
apmJSW <jeongsw@apm-coin.com>
2023-12-19 16:33:29 +0900

    feat: PlusMemberManager uuid -> userId 수정

--- apm-members-contracts/contracts/PlusMemberManager.sol
+++ apm-members-contracts/contracts/PlusMemberManager.sol
@@ -7,36 +7,36 @@ contract PlusMemberManager is ManagerRole {
     mapping(bytes32 => uint256) private memberIndex; // Track index of each member in memberList
     bytes32[] private memberList;
 
-    event PlusMemberAdded(bytes32 uuid);
-    event PlusMemberRemoved(bytes32 uuid);
+    event PlusMemberAdded(bytes32 userId);
+    event PlusMemberRemoved(bytes32 userId);
 
-    function addPlusMember(bytes32 uuid) public onlyManager {
-        require(!plusMembers[uuid], "Member already exists");
-        plusMembers[uuid] = true;
-        memberIndex[uuid] = memberList.length;
-        memberList.push(uuid);
-        emit PlusMemberAdded(uuid);
+    function addPlusMember(bytes32 userId) public onlyManager {
+        require(!plusMembers[userId], "Member already exists");
+        plusMembers[userId] = true;
+        memberIndex[userId] = memberList.length;
+        memberList.push(userId);
+        emit PlusMemberAdded(userId);
     }
 
-    function removePlusMember(bytes32 uuid) public onlyManager {
-        require(plusMembers[uuid], "Member does not exist");
-        plusMembers[uuid] = false;
+    function removePlusMember(bytes32 userId) public onlyManager {
+        require(plusMembers[userId], "Member does not exist");
+        plusMembers[userId] = false;
 
         // Remove member from memberList
-        uint256 index = memberIndex[uuid];
+        uint256 index = memberIndex[userId];
         bytes32 lastMember = memberList[memberList.length - 1];
         memberList[index] = lastMember; // Move the last member to the removed spot
         memberIndex[lastMember] = index;
         memberList.length--; // Reduce the array length
 
-        emit PlusMemberRemoved(uuid);
+        emit PlusMemberRemoved(userId);
     }
 
     function getPlusMemberCount() public view returns (uint256) {
         return memberList.length;
     }
 
-    function isPlusMember(bytes32 uuid) public view returns (bool) {
-        return plusMembers[uuid];
+    function isPlusMember(bytes32 userId) public view returns (bool) {
+        return plusMembers[userId];
     }
 }
