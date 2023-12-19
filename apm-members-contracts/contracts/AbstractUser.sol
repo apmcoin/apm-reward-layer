6e0165da35b7f08cf867a3e3ecff835299c42812
apmJSW <jeongsw@apm-coin.com>
2023-12-19 16:30:12 +0900

    feat: removeItemToInventory 함수 추가

--- apm-members-contracts/contracts/AbstractUser.sol
+++ apm-members-contracts/contracts/AbstractUser.sol
@@ -3,13 +3,13 @@ pragma solidity 0.5.6;
 import "./ManagerRole.sol";
 
 contract AbstractUser {
-  bytes32 public userId;
   ManagerRole public managerContract;
-  
-  mapping(uint256 => bool) public userInventory;
+
+  bytes32 public userId;
+  mapping(uint256 => bool) public inventory;
 
   modifier onlyManager() {
-    require(managerContract.isManager(msg.sender), "User: caller is nota manager");
+    require(managerContract.isManager(msg.sender), "User: caller is not a manager");
     _;
   }
 
@@ -19,10 +19,14 @@ contract AbstractUser {
   }
 
   function addItemToInventory(uint256 itemId) public onlyManager {
-    userInventory[itemId] = true;
+    inventory[itemId] = true;
+  }
+
+  function removeItemToInventory(uint256 itemId) public onlyManager {
+    delete inventory[itemId];
   }
 
   function checkInventory(uint256 itemId) public view returns (bool) {
-    return userInventory[itemId];
+    return inventory[itemId];
   }
 }
