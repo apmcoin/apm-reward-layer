59794b8c5e3e973e110b9c549a0d9044e8310240
apmJSW <jeongsw@apm-coin.com>
2023-12-20 17:53:34 +0900

    feat: AbstractUser에 활성화, 비활성화 유저 구분 로직 추가

--- apm-members-contracts/contracts/AbstractUser.sol
+++ apm-members-contracts/contracts/AbstractUser.sol
@@ -4,21 +4,28 @@ import "./ManagerRole.sol";
 
 contract AbstractUser is ManagerRole {
   bytes32 private userId;
-  mapping(uint256 => bytes32) private inventory;
+  bool private isActive;
+  mapping(bool => mapping(uint256 => bytes32)) private inventory;
 
   constructor(bytes32 _userId) public {
     userId = _userId;
+    isActive = true;
   }
 
-  function addItemToInventory(uint256 itemId, bytes32 value) public onlyManager {
-    inventory[itemId] = value;
+  function addItemToInventory(uint256 itemId, bytes32 url) public onlyManager {
+    inventory[isActive][itemId] = url;
   }
 
   function removeItemToInventory(uint256 itemId) public onlyManager {
-    delete inventory[itemId];
+    delete inventory[isActive][itemId];
   }
 
   function checkInventory(uint256 itemId) public view returns (bytes32) {
-    return inventory[itemId];
+    return inventory[isActive][itemId];
+  }
+
+  function destroy() public onlyManager {
+    isActive = false;
+
   }
 }
