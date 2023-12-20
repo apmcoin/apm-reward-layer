4908f03e9b8a2ccfa25f4f4fce218f1e5507198b
apmJSW <jeongsw@apm-coin.com>
2023-12-20 18:33:31 +0900

    feat: MembersPoint 발행, 회수 이벤트 추가

--- apm-members-contracts/contracts/MembersPoint.sol
+++ apm-members-contracts/contracts/MembersPoint.sol
@@ -6,11 +6,18 @@ import "./ManagerRole.sol";
 contract MembersPoint is ERC20, ManagerRole {
   constructor() public ERC20() {}
 
+  event MembersPointIssued(address userCA, uint256 amount);
+  event MembersPointCollected(address userCA, uint256 amount);
+
   function mint(address userCA, uint256 amount) public onlyManager {
     _mint(userCA, amount);
+
+    emit MembersPointIssued(userCA, amount);
   }
 
   function burn(address userCA, uint256 amount) public onlyManager {
     _burn(userCA, amount);
+
+    emit MembersPointCollected(userCA, amount);
   }
 }
