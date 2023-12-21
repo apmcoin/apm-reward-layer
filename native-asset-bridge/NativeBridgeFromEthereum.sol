91a26656fb52dbbf5204939555c9cfa4bd013265
jeon <46668752+john-ver@users.noreply.github.com>
2023-12-21 15:29:52 +0900

    Add Event Logging and Zero Address Validation

--- native-asset-bridge/NativeBridgeFromEthereum.sol
+++ native-asset-bridge/NativeBridgeFromEthereum.sol
@@ -6,19 +6,28 @@ import "./rapm-contracts/access/roles/OwnableRole.sol";
 contract NativeBridgeFromEthereum is OwnableRole {
     IERC20 private APM = IERC20(0xC8C424B91D8ce0137bAB4B832B7F7D154156BA6c);
 
-    // Function to transfer ETH to the RAPM chain.
-    // The asset transfer is fully completed only after verification by the nodes.
+    // Events for logging
+    event SentToRAPM(address indexed sender, uint256 amount);
+    event RetrievedFromRAPM(address indexed to, uint256 amount);
+    event EmergencyERC20Recovered(address indexed token, uint256 amount);
+
+    // Function to transfer ETH to the RAPM chain
     function sendToRAPM(uint256 amount) public {
         require(APM.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        emit SentToRAPM(msg.sender, amount);
     }
 
-    // Function to return assets from RAPM to the Ethereum chain.
-    // The asset transfer is fully completed only after verification by the nodes.
+    // Function to return assets from RAPM to the Ethereum chain
     function retrieveFromRAPM(address to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Invalid address: zero address");
         require(APM.transfer(to, amount), "Transfer failed");
+        emit RetrievedFromRAPM(to, amount);
     }
 
+    // Function to recover ERC20 tokens
     function emergencyRecoverERC20(IERC20 token, uint256 amount) public onlyOwner {
+        require(address(token) != address(0), "Invalid token: zero address");
         require(token.transfer(msg.sender, amount), "Recovery failed");
+        emit EmergencyERC20Recovered(address(token), amount);
     }
 }
--- native-asset-bridge/NativeBridgeFromRAPM.sol
+++ native-asset-bridge/NativeBridgeFromRAPM.sol
@@ -4,18 +4,25 @@ import "./rapm-contracts/access/roles/OwnableRole.sol";
 
 contract NativeBridgeFromRAPM is OwnableRole {
 
-    // Fallback function to receive RAPM. It is called automatically when RAPM is sent to this contract.
+    // Events for logging
+    event RAPMReceived(address indexed sender, uint256 amount);
+    event RAPMTransferred(address indexed to, uint256 amount);
+
+    // Fallback function to receive RAPM
     function() external payable {
         require(msg.value > 0, "No RAPM received");
+        emit RAPMReceived(msg.sender, msg.value);
     }
 
-    // Function to transfer RAPM to another address. Can only be called by the owner.
+    // Function to transfer RAPM to another address
     function transferRAPM(address payable to, uint256 amount) public onlyOwner {
+        require(to != address(0), "Invalid address: zero address");
         require(address(this).balance >= amount, "Insufficient RAPM balance");
         to.transfer(amount);
+        emit RAPMTransferred(to, amount);
     }
 
-    // Function to check the current RAPM balance of the contract.
+    // Function to check the current RAPM balance of the contract
     function getRAPMBalance() public view returns (uint256) {
         return address(this).balance;
     }
