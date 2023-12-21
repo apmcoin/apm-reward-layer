b59e613a1e2c017671eb33a3d814fc1e9b845847
apmJSW <jeongsw@apm-coin.com>
2023-12-21 18:49:58 +0900

    test: UserFactory createUser함수 테스트 코드 작성

--- apm-members-contracts/test/UserFactory.ts
+++ apm-members-contracts/test/UserFactory.ts
@@ -1,24 +1,55 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { UserFactory } from './../typechain-types/UserFactory';
+import { utils } from 'ethers';
 
-describe('PlusMemberManager', () => {
-  // let UserFactoryFactory: any;
+describe('UserFactory', () => {
+  let UserFactoryFactory: any;
 
-  // let user: any;
-  // let userFactory: UserFactory;
+  let sbtDeployer: any;
+  let userUuid: string;
+  let userFactory: UserFactory;
   
-  // before(async () => {
-  //   [[user], UserFactoryFactory] = await Promise.all([
-  //     ethers.getSigners(), 
-  //     ethers.getContractFactory("UserFactory"),
-  //   ]);
-  // })
+  before(async () => {
+    [sbtDeployer] = await ethers.getSigners(), 
+    UserFactoryFactory = await ethers.getContractFactory("UserFactory", sbtDeployer.getAddress());
+  })
 
-  // beforeEach(async () => {
-  //   userFactory = await UserFactoryFactory.deploy();
-  // });
+  describe('createUser', () => {
+    const zeroAddress = '0x0000000000000000000000000000000000000000';
 
-  // it('통과한다', async () => {
-  // });
+    before(() => {
+      userUuid = utils.formatBytes32String('user_uuid_for_test');
+    })
+    beforeEach(async () => {
+      userFactory = await UserFactoryFactory.deploy();
+    });
+  
+    it('throws error if sender is not a manager', async () => {
+      await userFactory.removeManager(sbtDeployer.getAddress());
+
+      try {
+        await userFactory.createUser(userUuid);
+      } catch (e: any) {
+        expect(e.message).contain('caller does not have the Manager role');
+      }
+    });
+
+    it('throws error if userCA has already tokenId', async () => {
+      await userFactory.createUser(userUuid);
+
+      try {
+        await userFactory.createUser(userUuid);
+      } catch (e: any) {
+        expect(e.message).contain('userId already exists');
+      }
+    });
+
+    it('passes', async () => {
+      const tx = await userFactory.createUser(userUuid);
+      await tx.wait()
+      
+      expect(await userFactory.getUserCA(userUuid) !== zeroAddress).eq(true)
+    })
+  })
 })
