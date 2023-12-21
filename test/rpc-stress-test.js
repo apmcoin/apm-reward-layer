40a70b158976021a55a05e585b8a82b64858ccef
jeon <46668752+john-ver@users.noreply.github.com>
2023-12-22 02:58:08 +0900

    Web3 to Ethers

--- test/rpc-stress-test.js
+++ test/rpc-stress-test.js
@@ -1,50 +1,31 @@
 require('dotenv').config();
-const { Web3 } = require('web3');
+const { ethers } = require('ethers');
 
-const web3 = new Web3(process.env.RPC_URL);
-const senderPrivateKey = process.env.PRIVATE_KEY;
-const fromAddress = web3.eth.accounts.privateKeyToAccount(senderPrivateKey).address;
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-// nonce 값을 초기화하는 별도의 비동기 함수
-async function initializeNonce() {
-    return web3.eth.getTransactionCount(fromAddress, 'pending');
-    return Number(nonce); // nonce를 숫자로 명시적 변환
-}
-
-// ETH 전송 함수
-const sendEth = async (nonce) => {
+const sendEth = async () => {
     try {
-        const toAddress = web3.utils.toHex(web3.utils.randomHex(20));
-        const amount = web3.utils.toWei(`${(Math.random() * 0.09 + 0.01).toFixed(2)}`, 'ether');
+        const toAddress = ethers.utils.hexlify(ethers.utils.randomBytes(20));
+        const amount = ethers.utils.parseUnits(`${(Math.random() * 0.09 + 0.01).toFixed(2)}`, 'ether');
 
         const tx = {
-            from: fromAddress,
             to: toAddress,
             value: amount,
-            gas: 21000,
-            gasPrice: web3.utils.toWei('20', 'gwei'),
-            nonce: nonce,
-            chainId: 276
+            gasLimit: ethers.utils.hexlify(21000),
+            gasPrice: ethers.utils.parseUnits('20', 'gwei')
         };
 
-        const signedTx = await web3.eth.accounts.signTransaction(tx, senderPrivateKey);
-        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        const transaction = await wallet.sendTransaction(tx);
+        const receipt = await transaction.wait();
 
-        console.log(`Success: ${receipt.status}, TxID: ${receipt.transactionHash}, Amount: ${web3.utils.fromWei(amount, 'ether')} RAPM, To: ${toAddress}`);
-        return nonce + 1;
+        console.log(`Success: ${receipt.status}, TxID: ${receipt.transactionHash}, Amount: ${ethers.utils.formatUnits(amount, 'ether')} RAPM, To: ${toAddress}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         if (error.message.includes('ETIMEDOUT')) {
-            setTimeout(() => sendEth(nonce), 3000); // 3초 후 재시도
+            setTimeout(sendEth, 3000); // 3초 후 재시도
         }
-        return nonce + 1;
     }
 };
 
-// nonce 값을 초기화하고, sendEth 함수를 반복적으로 호출
-(async () => {
-    let nonce = await initializeNonce();
-    setInterval(async () => {
-        nonce = await sendEth(nonce);
-    }, 500);
-})();
+setInterval(sendEth, 500);
