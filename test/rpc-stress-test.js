4452fe332ea93dccc933390357d5bb41b8784f7d
jeon <46668752+john-ver@users.noreply.github.com>
2023-12-22 02:38:41 +0900

    Update rpc-stress-test.js

--- test/rpc-stress-test.js
+++ test/rpc-stress-test.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const { Web3 } = require('web3');
 
-// 환경 변수에서 RPC URL과 개인 키, 체인 ID를 가져옵니다.
 const web3 = new Web3(process.env.RPC_URL);
 const senderPrivateKey = process.env.PRIVATE_KEY;
 const fromAddress = web3.eth.accounts.privateKeyToAccount(senderPrivateKey).address;
 
+// nonce 값을 초기화하는 별도의 비동기 함수
+async function initializeNonce() {
+    return await web3.eth.getTransactionCount(fromAddress, 'pending');
+}
+
 // ETH 전송 함수
-const sendEth = async () => {
+const sendEth = async (nonce) => {
     try {
-        const nonce = await web3.eth.getTransactionCount(fromAddress);
         const toAddress = web3.utils.toHex(web3.utils.randomHex(20));
         const amount = web3.utils.toWei(`${(Math.random() * 0.09 + 0.01).toFixed(2)}`, 'ether');
 
@@ -18,18 +21,22 @@ const sendEth = async () => {
             to: toAddress,
             value: amount,
             gas: 21000,
-            gasPrice: web3.utils.toWei('50', 'gwei'),
+            gasPrice: web3.utils.toWei('20', 'gwei'),
             nonce: nonce,
             chainId: 276
         };
 
         const signedTx = await web3.eth.accounts.signTransaction(tx, senderPrivateKey);
-        await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-            .on('receipt', console.log);
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+        console.log(`Success: ${receipt.status}, TxID: ${receipt.transactionHash}, Amount: ${web3.utils.fromWei(amount, 'ether')} ETH, To: ${toAddress}`);
+
     } catch (error) {
-        console.error(error);
+        console.error(`Error: ${error.message}`);
     }
 };
 
-// 1초마다 ETH 전송
-setInterval(sendEth, 1000);
+// nonce 값을 초기화하고, sendEth 함수를 반복적으로 호출
+initializeNonce().then((nonce) => {
+    setInterval(() => sendEth(nonce++), 1000);
+});
