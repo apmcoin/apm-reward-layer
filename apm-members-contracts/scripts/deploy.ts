ca02538a59ead35bedec932fd324a8c0bba6df1f
apmJSW <jeongsw@apm-coin.com>
2023-12-21 19:00:27 +0900

    feat: UserFactory deploy script 작성

--- apm-members-contracts/scripts/deploy.ts
+++ apm-members-contracts/scripts/deploy.ts
@@ -1,19 +1,24 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  // const plusMemberSbt = await ethers("PlusMemberSbt");
-  // const deployed = await plusMemberSbt.waitForDeployment();
-
+async function deplyPlusMemberSbt() {
   const PlusMemberSbt = await ethers.getContractFactory("PlusMemberSbt");
   const plusMemberSbt = await PlusMemberSbt.deploy();
   await plusMemberSbt.deployed()
   
-  console.log(plusMemberSbt.address);
+  console.log('PlusMemberSbt: ', plusMemberSbt.address);
+}
+
+async function deplyUserFactory() {
+  const UserFactory = await ethers.getContractFactory("UserFactory");
+  const userFactory = await UserFactory.deploy();
+  await userFactory.deployed()
+  
+  console.log('UserFactory: ', userFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+deplyUserFactory().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
