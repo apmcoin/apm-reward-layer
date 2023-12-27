6f4da75d4a405149efe47a0a95253a30e31a3201
apmJSW <jeongsw@apm-coin.com>
2023-12-27 15:30:44 +0900

    feat: MembersPoint 토큰 이름, 심볼 등록 추가

--- apm-members-contracts/contracts/MembersPoint.sol
+++ apm-members-contracts/contracts/MembersPoint.sol
@@ -4,10 +4,19 @@ import "./rapm-contracts/token/ERC20/ERC20.sol";
 import "./ManagerRole.sol";
 
 contract MembersPoint is ERC20, ManagerRole {
-  constructor() public ERC20() {}
+  string private _name = "a.point";
+  string private _symbol = "POINT";
+   uint8 private _decimals = 18;
 
   event MembersPointIssued(address userCA, uint256 amount);
   event MembersPointCollected(address userCA, uint256 amount);
+    
+  constructor() public ERC20() {}
+
+  function setDetailed(string memory newName, string memory newSymbol) public onlyManager{
+    _name = newName;
+    _symbol = newSymbol;
+  }
 
   function mint(address userCA, uint256 amount) public onlyManager {
     _mint(userCA, amount);
@@ -19,5 +28,17 @@ contract MembersPoint is ERC20, ManagerRole {
     _burn(userCA, amount);
 
     emit MembersPointCollected(userCA, amount);
+  }
+
+  function name() public view returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view returns (string memory) {
+    return _symbol;
+  }
+
+  function decimals() public view returns (uint8) {
+    return _decimals;
   }
 }
