1adaacf4dc7e21afce87dcb2f7441c538ba8ff90
apmJSW <jeongsw@apm-coin.com>
2024-06-17 17:46:08 +0900

    fix: bus ride count 더하기 수정

--- apm-members-contracts/contracts/BusCardSbt.sol
+++ apm-members-contracts/contracts/BusCardSbt.sol
@@ -36,9 +36,9 @@ contract BusCard is ERC721Full, ManagerRole {
         _mintNext(userCA);
       }
 
-      totalRideCount += totalRideCount.add(1);
-      fromRideCount[from] += fromRideCount[from].add(1);
-      userRideCount[getTokenId(userCA)] += userRideCount[getTokenId(userCA)].add(1);
+      totalRideCount = totalRideCount.add(1);
+      fromRideCount[from] = fromRideCount[from].add(1);
+      userRideCount[getTokenId(userCA)] = userRideCount[getTokenId(userCA)].add(1);
 
       emit Rided(userCA, from, userType, timestmap);
     }
